59414f529f48b0639004872abfc2ca86b8a3e9dd
Maksim Koretskiy <maksim.koretskiy@saltpepper.ru>
2016-04-11 00:48:27 +0300

    Add basic jss-isolate example

---
+++ plugins/jss-isolate/simple/style.js
@@ -0,0 +1,12 @@
+(function() {
+  window.styles = {
+    'button': {
+      'font-size': '20px',
+      'background-color': '#50ee50',
+      'color': '#fff',
+      'border-radius': '3px',
+      'padding': '10px 20px',
+      'font-family': 'sans-serif',
+    }
+  }
+}())
